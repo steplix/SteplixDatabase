90f2a9923d67b92bffc01a5e7abfbabfa30400ea
Matias Duarte <matias@steplix.com>
2020-02-18 11:52:31 -0300

    modified transaction method to handle a pool of connections

--- core/database.js
+++ core/database.js
@@ -5,13 +5,6 @@ const P = require('bluebird');
 const mysql = require('mysql');
 
 function _connect (options) {
-    if (options.usePool) {
-        // Default connection limit to 100 
-        if (!options.connectionLimit) {
-            options.connectionLimit = 100;
-        }
-        return mysql.createPool(options);
-    }
     return mysql.createConnection(options);
 }
 
@@ -19,6 +12,27 @@ class Database {
     constructor (options) {
         this.options = options;
         this.connection = _connect(this.options);
+        if (this.options.usePool) {
+            this.poolOptions = options;
+            // Default connection limit to 100
+            if (!this.poolOptions.connectionLimit) {
+                this.poolOptions.connectionLimit = 100;
+                this.poolOptions.multipleStatements = true;
+            }
+            this.pool = mysql.createPool(this.poolOptions);
+        }
+    }
+
+    getConnection () {
+        if (this.options.usePool) {
+            return new P((resolve, reject) => {
+                this.pool.getConnection((error, connection) => {
+                    if (error) return reject(error);
+                    return resolve(connection);
+                });
+            });
+        }
+        return this.connection;
     }
 
     query (query, args = []) {
@@ -41,32 +55,32 @@ class Database {
         });
     }
 
-    transaction (callback, connection) {
+    async transaction (callback) {
+        // Transactions must be executed in new conections to avoid undesired COMMIT or ROLLBACK from another TRANSACTION
+        // Also queries inside the callback must be executed over the returned connection
+        let newconnection;
+        if (this.options.usePool) {
+            newconnection = await this.getConnection();
+        }
+        else {
+            newconnection = this.connection;
+        }
         return new P((resolve, reject) => {
-            const handle = connection => {
-                return connection.beginTransaction(error => {
-                    return P.resolve()
-                        .then(() => callback())
-                        .then(result => {
-                            return new P((resolve, reject) => {
-                                connection.commit(error => {
-                                    if (error) return reject(error);
-                                    return resolve(result);
-                                });
+            return newconnection.beginTransaction(error => {
+                if (error) return reject(error);
+                return P.resolve()
+                    .then(() => callback(newconnection))
+                    .then(result => {
+                        return new P((resolve, reject) => {
+                            newconnection.commit(error => {
+                                if (error) return reject(error);
+                                return resolve(result);
                             });
-                        })
-                        .then(resolve)
-                        .catch(error => connection.rollback(() => reject(error)));
-                });
-            };
-
-            if (this.options.usePool) {
-                return this.connection.getConnection((error, connection) => {
-                    if (error) return reject(error);
-                    return handle(connection);
-                });
-            }
-            return handle(this.connection);
+                        });
+                    })
+                    .then(resolve)
+                    .catch(error => newconnection.rollback(() => reject(error)));
+            });
         });
     }
 
